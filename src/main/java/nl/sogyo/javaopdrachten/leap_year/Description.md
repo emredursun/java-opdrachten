d2dea6a2b8c4e13d039d9bd41b1945bd75c52d97
emre <edursun@sogyo.nl>
2023-06-04 20:05:43 +0200

    Add Java program for checking leap year

---
+++ src/main/java/nl/sogyo/javaopdrachten/leap_year/Description.md
@@ -0,0 +1,16 @@
+## **Leap year (required)**
+
+Write a program that has a year as input and then check if this year is a leap year.
+
+A year is a leap year when:
+
+- every fourth year is a leap year.
+- a new century is only a leap year when it is dividable by 400.
+
+Do not use the standard date functionality to perform the check.
+
+### **Learning goals**
+
+- Creating and running a program
+- Conditional statements
+- Input and output
---
+++ src/main/java/nl/sogyo/javaopdrachten/leap_year/LeapYear.java
@@ -0,0 +1,31 @@
+package nl.sogyo.javaopdrachten.leap_year;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println("This Java program prompts the user to enter a year and determines whether it is a leap year or not.\n" +
+                            "The program uses a while loop to allow the user to try again if desired.\n");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean tryAgain = true;
+        while (tryAgain) {
+            System.out.print("Enter a year: ");
+            int year = scanner.nextInt();
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " is a leap year.");
+            } else {
+                System.out.println(year + " is not a leap year.");
+            }
+
+            System.out.print("Would you like to try again? (yes or no): ");
+            String response = scanner.next().toLowerCase();
+            if (response.equals("no")) {
+                tryAgain = false;
+            }
+        }
+    }
+}
+
+
