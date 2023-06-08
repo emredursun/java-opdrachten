18f6d5c8a13f8192f04dbca040a031b1ea7b9e1b
emre <edursun@sogyo.nl>
2023-06-08 11:21:41 +0200

    Add a new method which avoids when the user enters invalid input.

--- src/main/java/nl/sogyo/javaopdrachten/experimental_projects/ConditionalStatements.java
+++ src/main/java/nl/sogyo/javaopdrachten/experimental_projects/ConditionalStatements.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class IfStatement {
+public class ConditionalStatements {
     public static void main(String[] args) {
         printDayType();
     }
 
     public static void printDayType() {
-        System.out.println("Enter a day name...");
+        System.out.print("Enter a day name: ");
         Scanner scan = new Scanner(System.in);
         String dayName = scan.nextLine();
 
         if (!isValidDayName(dayName)) {
-            System.out.println("Invalid day name. Please enter a valid day name.");
+            System.out.println("Invalid day name. Try again!");
             printDayType();
             return;
         }
@@ -27,20 +27,16 @@ public class IfStatement {
                 dayName.equalsIgnoreCase("Friday");
 
         if (isWeekdayName) {
-            System.out.println("Weekday");
+            System.out.println(dayName.toUpperCase() + " is a Weekday!");
         } else {
             boolean isWeekendDayName = dayName.equalsIgnoreCase("Saturday") ||
                     dayName.equalsIgnoreCase("Sunday");
             if (isWeekendDayName) {
-                System.out.println("Weekend Day");
+                System.out.println(dayName.toUpperCase() + " is a Weekend Day!");
             }
         }
 
-        System.out.println("Would you like to try again? (yes or no): ");
-        String response = scan.next().toLowerCase();
-        if (response.equals("yes")) {
-            printDayType();
-        }
+        tryAgain();
     }
 
     public static boolean isValidDayName(String dayName) {
@@ -54,5 +50,19 @@ public class IfStatement {
                 "sunday"
         );
         return validDayNames.contains(dayName.toLowerCase());
+    }
+
+    public static void tryAgain(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Would you like to try again? (yes or no): ");
+        String response = scan.next().toLowerCase();
+        if (response.equals("yes")) {
+            printDayType();
+        } else if (response.equals("no")) {
+            System.out.println("Execution stopped!");
+        } else {
+            System.out.println("Invalid input. Try again!");
+            tryAgain();
+        }
     }
 }
