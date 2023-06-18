b30fbd0f7c09faf9560498e8c1c476e847df19d9
emre <edursun@sogyo.nl>
2023-06-18 20:53:12 +0200

    Last version of the Exceptional Users Assignment

--- src/main/java/nl/sogyo/javaopdrachten/exceptional_users/User.java
+++ src/main/java/nl/sogyo/javaopdrachten/exceptional_users/User.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class User {
-
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter a username");
@@ -33,56 +32,30 @@ public class User {
     }
 
     public static boolean isValidPassword(String password) {
-        // Password should consist of one lowercase character, one uppercase character,
-        // one number, and must be at least 7 characters long.
+        // Password should consist of one lowercase character, one uppercase character, one number, and must be at least 7 characters long.
         String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{7,}$";
         return Pattern.matches(regex, password);
     }
 
-    public static void registerUser(String username, String password) throws SQLException {
-        // Establish the connection of MySQL database
+    public static void registerUser(String username, String password) {
         String url = "jdbc:mysql://localhost:3306/jdbc_tech?useSSL=false";
         String dbUserName = "root";
         String dbPassword = "25524";
-        Connection connection = DriverManager.getConnection(url, dbUserName, dbPassword);
-
-        String QUERY = "INSERT INTO users (id, username, password) VALUES (?, ?, ?)";
-
-        // Create a prepared statement
-        try (PreparedStatement preStmt = connection.prepareStatement(QUERY)) {
-            Random random = new Random();
-            int id = random.nextInt(1000);
-
-            // Now enter the users data as many as you want
+        try (Connection connection = DriverManager.getConnection(url, dbUserName, dbPassword)) {
+            String QUERY = "INSERT INTO users (id, username, password) VALUES (?, ?, ?)";
+            PreparedStatement preStmt = connection.prepareStatement(QUERY);
+            int id = new Random().nextInt(1000);
             preStmt.setInt(1, id);
             preStmt.setNString(2, username);
             preStmt.setNString(3, password);
-
-            int rowsAffected = preStmt.executeUpdate();
-
-            if (rowsAffected > 0) {
+            int rowsInserted = preStmt.executeUpdate();
+            if (rowsInserted > 0) {
                 System.out.println(username + " has successfully registered in the database.");
             } else {
                 System.out.println(username + " is failed to register in the database!");
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
-
-    //    public static void validatePassword(String password) throws Exception {
-//        if (password.length() < 8) {
-//            throw new Exception("Password must be at least 8 characters long.");
-//        }
-//        if (!password.matches(".*[a-z].*")) {
-//            throw new Exception("Password must contain at least one lowercase letter.");
-//        }
-//        if (!password.matches(".*[A-Z].*")) {
-//            throw new Exception("Password must contain at least one uppercase letter.");
-//        }
-//        if (!password.matches(".*\\d.*")) {
-//            throw new Exception("Password must contain at least one number.");
-//        }
-//    }
-
 }
